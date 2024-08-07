51989658f2a611227899e95502f95c98a76923df
mariegrace <mariegracebmg@gmail.com>
2024-08-07 15:35:08 +0200

    Add the title a wecoming message

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,11 @@
+"use client"
+import Hero from "@/components/Home/Hero";
 import React from "react";
 
 export default function Home() {
   return (
-    <div>
-      <h2> Main Page</h2>
+    <div className="p-5 sm:px-10 md:px-20">
+      <Hero />
     </div>
   );
 }
---
+++ components/Home/Hero.tsx
@@ -0,0 +1,19 @@
+import React from 'react'
+
+function Hero() {
+  return (
+    <div className='grid grid-cols-1 md:grid-cols-2'>
+      <div>
+        <h2>Find Your Perfect Ride in Minutes</h2>
+        <p>Discover the freedom of seamless car rentals. Choose from a variety of vehicles,
+          enjoy competitive rates, and hit the road with confidence.</p>
+          <button>Explore Cars</button>
+      </div>
+      <div>
+
+      </div>
+    </div>
+  )
+}
+
+export default Hero;
