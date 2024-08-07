311e32bd9d4edf6d3738abf1f6e516f670bfaa8f
mariegrace <mariegracebmg@gmail.com>
2024-08-07 15:49:25 +0200

    Add the home page image

--- components/Home/Hero.tsx
+++ components/Home/Hero.tsx
@@ -1,4 +1,6 @@
-import React from 'react'
+import Image from 'next/image';
+import React from 'react';
+import { FaArrowRight } from "react-icons/fa";
 
 function Hero() {
   return (
@@ -7,10 +9,14 @@ function Hero() {
         <h2>Find Your Perfect Ride in Minutes</h2>
         <p>Discover the freedom of seamless car rentals. Choose from a variety of vehicles,
           enjoy competitive rates, and hit the road with confidence.</p>
-          <button>Explore Cars</button>
+          <button>Explore Cars <FaArrowRight /> </button>
       </div>
       <div>
-
+        <Image src='/HomePImage.jpeg'
+        alt='Hero'
+        width={400}
+        height={500}
+        className='border-green-700' />
       </div>
     </div>
   )
